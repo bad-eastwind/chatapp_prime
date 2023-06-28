f38817d78bea138915620c8365c6da245d37bf8d
Diasha1998 <diashadas19@gmail.com>
2023-06-28 22:51:11 +0530

    updated by diasha, 28th june

--- app/UIcomponents/Chatbottom.jsx
+++ app/UIcomponents/Chatbottom.jsx
@@ -1,32 +1,42 @@
-'use client'
-import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
-import AttachFileIcon from '@mui/icons-material/AttachFile';
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Fab from '@mui/material/Fab';
-import { styled } from '@mui/system';
+"use client";
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import SendIcon from '@mui/icons-material/Send';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import { styled } from "@mui/system";
 
 const ExtendedFab = styled(Fab)({
-    width: '600px', // Set the desired width here
-    height: '60px',
+  width: "550px", // Set the desired width here
+  height: "60px",
+  display: "flex",
+  justifyContent: "flex-start",
+  alignItems: "center",
+});
+
+const FabContainer1 = styled('div')({
     display: 'flex',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
+    gap: '20px', // Set the desired gap between the FABs
   });
 
-
-  export default function Chatbottom() {
-    return (
-      <div>
-        <Box sx={{ marginLeft: "40px", "& > :not(style)": { m: 1 } }}>
-          <ExtendedFab variant="extended"  size="large">
-            <span style={{ marginRight: "8px" }}>
+export default function Chatbottom() {
+  return (
+    <div>
+        <FabContainer1>
+      <Box sx={{ marginLeft: "40px", "& > :not(style)": { m: 1 } }}>
+        <ExtendedFab variant="extended" size="large">
+          <span style={{ marginRight: "8px" }}>
             <SentimentSatisfiedAltIcon sx={{ mr: 1 }} />
             Type a message
-            <AttachFileIcon  sx={{ mr: 1 }} />
-            </span>
-          </ExtendedFab>
-        </Box>
-      </div>
-    );
-  }
+            <AttachFileIcon sx={{ mr: 1 }} />
+          </span>
+        </ExtendedFab>
+        <Fab color="primary" aria-label="add">
+        <SendIcon />
+        </Fab>
+      </Box>
+      </FabContainer1>
+    </div>
+  );
+}
