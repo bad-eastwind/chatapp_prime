ec443976905a5be07105a5ab99e59a8f12889e02
Diasha1998 <diashadas19@gmail.com>
2023-06-29 00:00:15 +0530

    updated by diasha 28th june

--- app/UIcomponents/Chatbg.jsx
+++ app/UIcomponents/Chatbg.jsx
@@ -1,10 +1,17 @@
 'use client'
+import SendIcon from '@mui/icons-material/Send';
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Typography from "@material-ui/core/Typography";
 import Chatbox from './Chatbox'
 import Chatbottom from './Chatbottom';
+import { styled } from "@mui/system"
+
+const FabContainer1 = styled('div')({
+  display: 'flex',
+  gap: '5px', // Set the desired gap between the FABs
+});
 
 export default function Chatbg() {
   return (
